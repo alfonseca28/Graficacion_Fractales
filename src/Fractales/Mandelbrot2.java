ba4bb0c7782b4d0b7d5387e712522663d79f60c7
Aaron Alfonseca <alfonzka_13@hotmail.com>
2022-02-27 21:31:43 -0600

    Continuo probando combinaciones con los fractales de Mandelbrot

--- src/Fractales/Mandelbrot2.java
+++ src/Fractales/Mandelbrot2.java
@@ -17,7 +17,7 @@ import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class Mandelbrot2 extends JFrame {
 
-    private final int MAX_ITER = 650;
+    private final int MAX_ITER = 1234;
     private final double ZOOM = 250;
     private BufferedImage I;
     private double zx, zy, cX, cY, tmp;
@@ -28,19 +28,19 @@ public class Mandelbrot2 extends JFrame {
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         I = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
-        for (int y = 3; y < getHeight(); y++) {
-            for (int x = 3; x < getWidth(); x++) {
+        for (int y = 0; y < getHeight(); y++) {
+            for (int x = 0; x < getWidth(); x++) {
                 zx = zy = 0;
                 cX = (x - 450) / ZOOM;
                 cY = (y - 300) / ZOOM;
                 int iter = MAX_ITER;
-                while (zx * zx + zy * zy < 4 && iter > 0) {
-                    tmp = Math.pow(zx, 2) - Math.pow(zy, 2) + cX;
-                    zy = 2.0 * zx * zy + cY;
+                while (zx * zx + zy * zy < 9 && iter > 0) {
+                    tmp = zx * zx - zy * zy + cX;
+                    zy = 3.0 * zx * zy + cY;
                     zx = tmp;
                     iter--;
                 }
-                I.setRGB(x, y, iter | (iter << 11));
+                I.setRGB(x, y, iter | (iter << 14));
             }
         }
     }
